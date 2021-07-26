a3fcbf9a46116533d5c94758a564d32c7d6d4bf5
gabiap96 <apostol.gabriel90@gmail.com>
2021-07-26 10:42:53 +0300

    delete consolelog

--- src/components/CSVContent/Context/CSVData.context.js
+++ src/components/CSVContent/Context/CSVData.context.js
@@ -69,7 +69,6 @@ export const CSVDataProvider = ({ children }) => {
             })
             return arrayRow
         })
-        console.log("csvData:", arrayRows)
         return arrayRows;
     }
 
